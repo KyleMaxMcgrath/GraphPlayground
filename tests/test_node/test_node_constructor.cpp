fee03ff3a42e9e64e7e1cf3c81b012076e6cba28
Kyle McGrath <maxwell1@Yoris-PowerPC-One.home>
2017-10-13 22:54:07 -0400

    Add duration to tests.

--- tests/test_node/test_node_constructor.cpp
+++ tests/test_node/test_node_constructor.cpp
@@ -11,27 +11,30 @@
 #include <climits>
 #include <cmath>
 #include "../../graph/node.h"
+#include <chrono>
 
 /*
  * Simple C++ Test Suite
  */
 
+using namespace std;
+
 void test1() {
-    std::cout << "Test node constructor once." << std::endl;
+    cout << "Test node constructor once." << endl;
     node * n = new node(0);
     if(n->id.compare("BAAAA") != 0) {
-        std::cout << "%TEST_FAILED% time=0 testname=test1 message=constructor failed on first attempt." << std::endl;
+        cout << "%TEST_FAILED% time=0 testname=test1 message=constructor failed on first attempt." << endl;
     }
     delete n;
 }
 
 void test2() {
-    std::cout << "Test that the node constructor does not overflow prematurely (expect up to one id overflow)" << std::endl;
+    cout << "Test that the node constructor does not overflow prematurely (expect up to one id overflow)" << endl;
     
     for(int i = 0; i < 26*26*26*26*26-1; i++) {
         node * n = new node(0);
         if(n->id.compare("BAAAA") == 0)
-            std::cout << "%TEST_FAILED% time=0 testname=test2 message=getId() overflowed prematurely." << std::endl;
+            cout << "%TEST_FAILED% time=0 testname=test2 message=getId() overflowed prematurely." << endl;
         delete n;
     }
     
@@ -39,53 +42,67 @@ void test2() {
 }
 
 void test3() {
-    std::cout << "Test that node constructor overflows safely (expect two id overflows)." << std::endl;
+    cout << "Test that node constructor overflows safely (expect two id overflows)." << endl;
     try {
-        for(int i = 0; i < 2*26*26*26*26*26; i++) {
+        for(int i = 0; i < 2*26*26*26*26*26+1; i++) {
             node * n = new node(0);
             delete n;
         }
-    } catch(std::exception e) {
-        std::cerr << "%TEST_FAILED% time=0 testname=test3 message=getId() overflowed unsafely:" << e.what() << "." << std::endl;
+    } catch(exception e) {
+        cerr << "%TEST_FAILED% time=0 testname=test3 message=getId() overflowed unsafely:" << e.what() << "." << endl;
     }
 }
 
 void test4() {
-    std::cout << "Test that node constructor can handle values INT_MIN and INT_MAX." << std::endl;
+    cout << "Test that node constructor can handle values INT_MIN and INT_MAX." << endl;
 
     node * n1 = new node(INT_MIN);
-    if(n1->value != INT_MIN)
-            std::cerr << "%TEST_FAILED% time=0 testname=test4 message=node constructor did not handle value=" << 2147483647 << " correctly." << std::endl;
-    delete n1;
     node * n2 = new node(INT_MAX);
-    if(n2->value != INT_MAX)
-            std::cerr << "%TEST_FAILED% time=0 testname=test4 message=node constructor did not handle value=" << 2147483647 << " correctly." << std::endl;
+    if(n1->value != INT_MIN || n2->value != INT_MAX)
+            cerr << "%TEST_FAILED% time=0 testname=test4 message=node constructor did not handle value=" << 2147483647 << " correctly." << endl;
+    delete n1;
     delete n2;
 
-    
 }
 
 int main(int argc, char** argv) {
-    std::cout << "%SUITE_STARTING% test_node_constructor" << std::endl;
-    std::cout << "%SUITE_STARTED%" << std::endl;
+    chrono::steady_clock clock;
+    auto suiteStart = chrono::steady_clock::now();
+    cout << "%SUITE_STARTING% test_node_constructor" << endl;
+    cout << "%SUITE_STARTED%" << endl;
 
-    std::cout << "%TEST_STARTED% test1 " << std::endl;
+    auto start = chrono::steady_clock::now();
+    cout << "%TEST_STARTED% test1\n" << endl;
     test1();
-    std::cout << "%TEST_FINISHED% time=0 test1" << std::endl;
+    
+    auto end = chrono::steady_clock::now();
+    chrono::duration<double> dur = chrono::duration_cast<chrono::duration<double>>(end-start);
+    cout << "%TEST_FINISHED% time=" << dur.count() << " test1" << endl;
 
-    std::cout << "%TEST_STARTED% test2" << std::endl;
+    start = chrono::steady_clock::now();
+    cout << "%TEST_STARTED% test2\n" << endl;
     test2();
-    std::cout << "%TEST_FINISHED% time=0 test2" << std::endl;
+    end = chrono::steady_clock::now();
+    dur = chrono::duration_cast<chrono::duration<double>>(end-start);
+    cout << "%TEST_FINISHED% time=" << dur.count() << " test2" << endl;
     
-    std::cout << "%TEST_STARTED% test3" << std::endl;
+    start = chrono::steady_clock::now();
+    cout << "%TEST_STARTED% test3\n" << endl;
     test3();
-    std::cout << "%TEST_FINISHED% time=0 test3" << std::endl;
+    end = chrono::steady_clock::now();
+    dur = chrono::duration_cast<chrono::duration<double>>(end-start);
+    cout << "%TEST_FINISHED% time=" << dur.count() << " test3" << endl;
     
-    std::cout << "%TEST_STARTED% test4" << std::endl;
+    start = chrono::steady_clock::now();
+    cout << "%TEST_STARTED% test4\n" << endl;
     test4();
-    std::cout << "%TEST_FINISHED% time=0 test4" << std::endl;
+    end = chrono::steady_clock::now();
+    dur = chrono::duration_cast<chrono::duration<double>>(end-start);
+    cout << "%TEST_FINISHED% time=" << dur.count() << " test4" << endl;
 
-    std::cout << "%SUITE_FINISHED% time=0" << std::endl;
+    auto suiteEnd = chrono::steady_clock::now();
+    dur = chrono::duration_cast<chrono::duration<double>>(suiteEnd-suiteStart);
+    cout << "%SUITE_FINISHED% time=" << dur.count() << " << endl";
 
     return (EXIT_SUCCESS);
 }
