33218ece0c619e002ba87723db5cf99463f680c2
Kyle McGrath <maxwell1@Yoris-PowerPC-One.home>
2017-10-14 13:05:07 -0400

    Test equals and test use of equals and hash in an unordered_set.

--- tests/test_node/test_node_hash.cpp
+++ tests/test_node/test_node_hash.cpp
@@ -6,9 +6,9 @@
  */
 
 #include <stdlib.h>
+#include <unordered_set>
 #include <iostream>
 #include <chrono>
-#include <set>
 #include "graph/node.h"
 
 /*
@@ -18,25 +18,54 @@
 using namespace std;
 
 void test1() {
-    std::cout << "test_node_hash test 1" << std::endl;
+    cout << "test_node_hash test1" << endl;
     node n1(0);
     try {
         node::hash(n1);
     } catch(exception e) {
-        std::cout << "%TEST_FAILED% testname=test2 (test_node_hash) message=hash does not complete without error" << std::endl;
+        cout << "%TEST_FAILED% testname=test2 (test_node_hash) message=hash does not complete without error" << endl;
     }
 }
 
 void test2() {
-    std::cout << "test_node_hash test 2" << std::endl;
+    cout << "test_node_hash test2" << endl;
     node n1(1);
     node n2(2);
     if(node::hash(n1) == node::hash(n2))
-        std::cout << "%TEST_FAILED% testname=test2 (test_node_hash) message=Two distinct nodes had equal hashes." << std::endl;
+        cout << "%TEST_FAILED% testname=test2 (test_node_hash) message=Two distinct nodes had equal hashes." << endl;
 }
 
 void test3() {
+    cout << "test_node_hash test3" << endl;
+    node n(1);
+    if(!node::equals(n, n))
+        cout << "%TEST_FAILED% testname=test3 (test_node_hash) message=node::equals is not reflexive." << endl;
+}
+
+void test4() {
+    cout << "test_node_hash test4" << endl;
+    node n1(1);
+    node n2(2);
+    if(node::equals(n1, n2))
+        cout << "%TEST_FAILED% testname=test4 (test_node_hash) message=node::equals returned true with two distinct nodes." << endl;
+}
+
+void test5() {
+    cout << "test_node_hash test5" << endl;
+    typedef decltype(&node::hash) nodeHash;
+    typedef decltype(&node::equals) nodeEq;
     
+    try {
+
+        unordered_set<node, nodeHash, nodeEq> nodeSet(42, &node::hash, &node::equals);
+
+        for(int i = 0; i < 100000; i++) {
+            node n(0);
+            nodeSet.insert(n);
+        }
+    } catch(exception e) {
+        cout << "%TEST_FAILED% testname=test5 (test_node_hash) message=Failed to use set without an exception." << endl;
+    }
 }
  
 
@@ -44,27 +73,48 @@ int main(int argc, char** argv) {
     auto clock = chrono::steady_clock();
     
     auto suiteStart = clock.now();
-    std::cout << "%SUITE_STARTING% test_node_hash" << std::endl;
-    std::cout << "%SUITE_STARTED%" << std::endl;
+    cout << "%SUITE_STARTING% test_node_hash" << endl;
+    cout << "%SUITE_STARTED%" << endl;
 
     auto start = clock.now();
-    std::cout << "%TEST_STARTED% test1 (test_node_hash)" << std::endl;
+    cout << "%TEST_STARTED% test1 (test_node_hash)" << endl;
     test1();
     auto end = clock.now();
     auto dur = chrono::duration<double>(end-start);
-    std::cout << "%TEST_FINISHED% time=" << dur.count() << " test1 (test_node_hash)" << std::endl;
+    cout << "%TEST_FINISHED% time=" << dur.count() << " test1 (test_node_hash)" << endl;
     
     start = clock.now();
-    std::cout << "%TEST_STARTED% test1 (test_node_hash)" << std::endl;
+    cout << "%TEST_STARTED% test2 (test_node_hash)" << endl;
     test2();
     end = clock.now();
     dur = chrono::duration<double>(end-start);
-    std::cout << "%TEST_FINISHED% time=" << dur.count() << " test2 (test_node_hash)" << std::endl;
+    cout << "%TEST_FINISHED% time=" << dur.count() << " test2 (test_node_hash)" << endl;
+    
+    start = clock.now();
+    cout << "%TEST_STARTED% test3 (test_node_hash)" << endl;
+    test3();
+    end = clock.now();
+    dur = chrono::duration<double>(end-start);
+    cout << "%TEST_FINISHED% time=" << dur.count() << " test3 (test_node_hash)" << endl;
+    
+    start = clock.now();
+    cout << "%TEST_STARTED% test4 (test_node_hash)" << endl;
+    test4();
+    end = clock.now();
+    dur = chrono::duration<double>(end-start);
+    cout << "%TEST_FINISHED% time=" << dur.count() << " test4 (test_node_hash)" << endl;
+    
+    start = clock.now();
+    cout << "%TEST_STARTED% test5 (test_node_hash)" << endl;
+    test5();
+    end = clock.now();
+    dur = chrono::duration<double>(end-start);
+    cout << "%TEST_FINISHED% time=" << dur.count() << " test5 (test_node_hash)" << endl;
 
     
     auto suiteEnd = clock.now();
     auto suiteDur= chrono::duration<double>(suiteEnd-suiteStart);
-    std::cout << "%SUITE_FINISHED% time=" << suiteDur.count() << std::endl;
+    cout << "%SUITE_FINISHED% time=" << suiteDur.count() << endl;
 
     return (EXIT_SUCCESS);
 }
