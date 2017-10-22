042aa43777c7c1c87b18d3cdc3f72d903008f568
Kyle McGrath <maxwell1@Yoris-PowerPC-One.home>
2017-10-22 10:58:49 -0400

    Test node::connect(node*).

--- tests/tests/test_node_neighbors.cpp
+++ tests/tests/test_node_neighbors.cpp
@@ -15,7 +15,7 @@
 
 using namespace std;
 
-// Test node::neighbors1.618033988749895
+// Test node::neighbors
 void test1() {
     std::cout << "test_node_neighbors test 1" << std::endl;
     
@@ -71,14 +71,66 @@ void test2() {
         it = (*it)->neighbors.cbegin();
     }
     
-}
+}// Test node::neighbors
 
 void test3() {
     std::cout << "test_node_neighbors test 3" << std::endl;
     
+    try {
+        node* n(1);
+        n->connect(new node(6));
+        n->connect(new node(1));
+        n->connect(new node(8));
+        n->connect(new node(0));
+        n->connect(new node(3));
+        n->connect(new node(3));
+        n->connect(new node(9));
+        n->connect(new node(8));
+        n->connect(new node(8));
+        n->connect(new node(7));
+        n->connect(new node(4));
+        if(n->degree() != 11)
+            std::cout << "%TEST_FAILED% time=0 testname=test3 (test_node_neighbors) message=node degree not correct" << std::endl;
+        delete n;
+    } catch(exception e) {
+        std::cout << "%TEST_FAILED% time=0 testname=test3 (test_node_neighbors) message=node::addNeighbors() or node::degree() caused exception" << std::endl;
+    }
+
+}
+
+void test4() {
+    std::cout << "test_node_neighbors test 4" << std::endl;
     
+    int values [] = {1, 6, 1, 8, 0, 3, 3, 9, 8, 8, 7, 4, 9, 8, 9, 5};
+    string id [] = {"AAAAB", "AAAAC", "AAAAD", "AAAAE",
+                    "AAAAF", "AAAAG", "AAAAH", "AAAAI",
+                    "AAAAJ", "AAAAK", "AAAAL", "AAAAM",
+                    "AAAAN", "AAAAO", "AAAAP", "AAAAQ",
+                    "AAAAR", "AAAAS", "AAAAT", "AAAAU"};
     
-    std::cout << "%TEST_FAILED% time=0 testname=test3 (test_node_neighbors) message=" << std::endl;
+    node* n00 = new node(1); node* n01 = new node(6); node* n02 = new node(1); node* n03 = new node(8);
+    node* n04 = new node(0); node* n05 = new node(3); node* n06 = new node(3); node* n07 = new node(9);
+    node* n08 = new node(8); node* n09 = new node(8); node* n10 = new node(7); node* n11 = new node(4);
+    node* n12 = new node(9); node* n13 = new node(8); node* n14 = new node(9); node* n15 = new node(5);
+    
+    n00->connect(n01); n01->connect(n02); n02->connect(n03);  n03->connect(n04); 
+    n04->connect(n05); n05->connect(n06); n06->connect(n07);  n07->connect(n08); 
+    n08->connect(n09); n09->connect(n10); n10->connect(n11);  n11->connect(n12); 
+    n12->connect(n13); n13->connect(n14); n14->connect(n15);
+    
+    auto it = n00->neighbors.cbegin();
+    int i = 0;
+    while((*it)->neighbors.size() != 0) {
+        if((*it)->value != values[i])
+            std::cout << "%TEST_FAILED% time=0 testname=test4 (test_node_neighbors) message=node value incorrect" << std::endl;
+        if((*it)->id != id[i])
+            std::cout << "%TEST_FAILED% time=0 testname=test4 (test_node_neighbors) message=node id incorrect" << std::endl;
+        it = (*it)->neighbors.cbegin();
+    }
+    
+    delete n00; delete n01; delete n02; delete n03; delete n04; delete n05; delete n06; delete n07;
+    delete n08; delete n09; delete n10; delete n11; delete n12; delete n13; delete n14; delete n15;
+    
 }
 
 // Test node::prune()
@@ -108,6 +160,21 @@ int main(int argc, char** argv) {
     end = clock.now();
     dur = chrono::duration<double>(end-start);
     cout << "%TEST_FINISHED% time=" << dur.count() << " test2 (test_node_neighbors)" << endl;
+    
+    start = clock.now();
+    cout << "%TEST_STARTED% test3 (test_node_neighbors)" << endl;
+    test3();
+    end = clock.now();
+    dur = chrono::duration<double>(end-start);
+    cout << "%TEST_FINISHED% time=" << dur.count() << " test3 (test_node_neighbors)" << endl;
+    
+    
+    start = clock.now();
+    cout << "%TEST_STARTED% test4 (test_node_neighbors)" << endl;
+    test4();
+    end = clock.now();
+    dur = chrono::duration<double>(end-start);
+    cout << "%TEST_FINISHED% time=" << dur.count() << " test4 (test_node_neighbors)" << endl;
     
     auto suiteEnd = clock.now();
     auto suiteDur = chrono::duration<double>(suiteEnd-suiteStart);
