2dbedb89dc91083e4024686c727e7a4f623a1d79
Kyle McGrath <maxwell1@Yoris-PowerPC-One.home>
2017-10-22 11:41:18 -0400

    Fix tests. Prepare for node::connect(node&)
    Pass node as a reference to node::connect(). In test 4 only delete n00,
    because when the other nodes leave scope shared_ptr deletes the other
    raw ptrs. In test 2 and 4, increment i appropriately.

--- tests/tests/test_node_neighbors.cpp
+++ tests/tests/test_node_neighbors.cpp
@@ -20,24 +20,27 @@ void test1() {
     std::cout << "test_node_neighbors test 1" << std::endl;
     
     try {
-        node n(1);
-        n.connect(node(6));
-        n.connect(node(1));
-        n.connect(node(8));
-        n.connect(node(0));
-        n.connect(node(3));
-        n.connect(node(3));
-        n.connect(node(9));
-        n.connect(node(8));
-        n.connect(node(8));
-        n.connect(node(7));
-        n.connect(node(4));
-        if(n.degree() != 11)
+        node n00(1); node n01(6); node n02(1); node n03(8);
+        node n04(0); node n05(3); node n06(3); node n07(9);
+        node n08(8); node n09(8); node n10(7); node n11(4);
+        n00.connect(n01);
+        n00.connect(n02);
+        n00.connect(n03);
+        n00.connect(n04);
+        n00.connect(n05);
+        n00.connect(n06);
+        n00.connect(n07);
+        n00.connect(n08);
+        n00.connect(n09);
+        n00.connect(n10);
+        n00.connect(n11);
+        if(n00.degree() != 11)
             std::cout << "%TEST_FAILED% time=0 testname=test1 (test_node_neighbors) message=node degree not correct" << std::endl;
 
     } catch(exception e) {
         std::cout << "%TEST_FAILED% time=0 testname=test1 (test_node_neighbors) message=node::addNeighbors() or node::degree() caused exception" << std::endl;
     }
+    node::resetId();
 
 }
 
@@ -61,17 +64,27 @@ void test2() {
     n08.connect(n09); n09.connect(n10); n10.connect(n11);  n11.connect(n12); 
     n12.connect(n13); n13.connect(n14); n14.connect(n15);
     
+    if(n00.value != values[0])
+        std::cout << "%TEST_FAILED% time=0 testname=test2 (test_node_neighbors) message=node value incorrect" << std::endl;
+    if(n00.id != id[0])
+        std::cout << "%TEST_FAILED% time=0 testname=test2 (test_node_neighbors) message=node value incorrect" << std::endl;
+
     auto it = n00.neighbors.cbegin();
-    int i = 0;
-    while((*it)->neighbors.size() != 0) {
+    int i = 1;
+
+    while((*it)->degree() != 0) {
+        cerr << i << endl;
         if((*it)->value != values[i])
             std::cout << "%TEST_FAILED% time=0 testname=test2 (test_node_neighbors) message=node value incorrect" << std::endl;
         if((*it)->id != id[i])
             std::cout << "%TEST_FAILED% time=0 testname=test2 (test_node_neighbors) message=node id incorrect" << std::endl;
         it = (*it)->neighbors.cbegin();
+        i++;
     }
     
-}// Test node::neighbors
+    node::resetId();
+    
+}
 
 void test3() {
     std::cout << "test_node_neighbors test 3" << std::endl;
@@ -92,6 +105,7 @@ void test3() {
         if(n->degree() != 11)
             std::cout << "%TEST_FAILED% time=0 testname=test3 (test_node_neighbors) message=node degree not correct" << std::endl;
         delete n;
+        node::resetId();
     } catch(exception e) {
         std::cout << "%TEST_FAILED% time=0 testname=test3 (test_node_neighbors) message=node::addNeighbors() or node::degree() caused exception" << std::endl;
     }
@@ -100,37 +114,44 @@ void test3() {
 
 void test4() {
     std::cout << "test_node_neighbors test 4" << std::endl;
-    
+
     int values [] = {1, 6, 1, 8, 0, 3, 3, 9, 8, 8, 7, 4, 9, 8, 9, 5};
     string id [] = {"AAAAB", "AAAAC", "AAAAD", "AAAAE",
                     "AAAAF", "AAAAG", "AAAAH", "AAAAI",
                     "AAAAJ", "AAAAK", "AAAAL", "AAAAM",
                     "AAAAN", "AAAAO", "AAAAP", "AAAAQ",
                     "AAAAR", "AAAAS", "AAAAT", "AAAAU"};
-    
+
     node* n00 = new node(1); node* n01 = new node(6); node* n02 = new node(1); node* n03 = new node(8);
     node* n04 = new node(0); node* n05 = new node(3); node* n06 = new node(3); node* n07 = new node(9);
     node* n08 = new node(8); node* n09 = new node(8); node* n10 = new node(7); node* n11 = new node(4);
     node* n12 = new node(9); node* n13 = new node(8); node* n14 = new node(9); node* n15 = new node(5);
-    
+
     n00->connect(n01); n01->connect(n02); n02->connect(n03);  n03->connect(n04); 
     n04->connect(n05); n05->connect(n06); n06->connect(n07);  n07->connect(n08); 
     n08->connect(n09); n09->connect(n10); n10->connect(n11);  n11->connect(n12); 
     n12->connect(n13); n13->connect(n14); n14->connect(n15);
-    
+
+    if(n00->value != values[0])
+        std::cout << "%TEST_FAILED% time=0 testname=test4 (test_node_neighbors) message=node value incorrect" << std::endl;
+    if(n00->id != id[0])
+        std::cout << "%TEST_FAILED% time=0 testname=test4 (test_node_neighbors) message=node value incorrect" << std::endl;
+
     auto it = n00->neighbors.cbegin();
-    int i = 0;
+    int i = 1;
     while((*it)->neighbors.size() != 0) {
         if((*it)->value != values[i])
             std::cout << "%TEST_FAILED% time=0 testname=test4 (test_node_neighbors) message=node value incorrect" << std::endl;
         if((*it)->id != id[i])
-            std::cout << "%TEST_FAILED% time=0 testname=test4 (test_node_neighbors) message=node id incorrect" << std::endl;
+            std::cout << "%TEST_FAILED% time=0 testname=test4 (test_node_neighbors) message=node id incorrect: " << (*it)->id << std::endl;
         it = (*it)->neighbors.cbegin();
+        i++;
     }
     
-    delete n00; delete n01; delete n02; delete n03; delete n04; delete n05; delete n06; delete n07;
-    delete n08; delete n09; delete n10; delete n11; delete n12; delete n13; delete n14; delete n15;
-    
+    delete n00;
+
+    node::resetId();
+
 }
 
 // Test node::prune()
